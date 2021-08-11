3b0b2a7fe49392bce7e50f28ff078ae7acec87a7
Daniel Beals <BealsD@delagelanden.com>
2021-08-11 14:17:54 -0500

    First Solution/Test

---
+++ Excercises/Excercises/FindMaxConsecutiveOnes.cs
@@ -0,0 +1,29 @@
+﻿namespace Excercises
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int max = 0, current = 0;
+            foreach (int number in nums)
+            {
+                if (number == 1)
+                {
+                    current++;
+                }
+                else
+                {
+                    max = (current > max) ? current : max;
+                    current = 0;
+                }
+                
+            }
+            return max = (current > max) ? current : max;
+        }
+    }
+}
---
+++ Excercises/Excercises/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 0, 1, 1, 1 };
+            Console.WriteLine( Solution.FindMaxConsecutiveOnes(nums));
+        }
+    }
+}
