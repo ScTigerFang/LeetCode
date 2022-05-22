fb9d87cb5dce36c066aa752a77a3eb08106da685
Daniel Beals <dbeals@vermeer.com>
2022-05-21 23:38:08 -0500

    Working on upstairs PC, been working on Coin Change problem

---
+++ Excercises/LeetCode/Medium/322 Coin Change.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/coin-change/
+    /// </summary>
+    public class Coin_Change_322
+    {
+        public static int CoinChange(int[] coins, int amount)
+        {
+            int minCoinsRequired = 0;
+            int amountLeft = amount;
+
+            if (amount == 0)
+                return 0;
+            if (coins.Min() > amount)
+            {
+                return -1;
+            }
+            foreach (var coin in coins)
+            {
+                Console.WriteLine("Trying to solve it with first solution");
+                var coins_copy = coins;
+                while (coins_copy.Length != 0 && amountLeft != 0)
+                {
+                    minCoinsRequired += amountLeft / coins_copy.Max();
+                    Console.WriteLine(amountLeft + "%" + coins_copy.Max().ToString() + " = " + amountLeft % coins_copy.Max());
+                    amountLeft = amountLeft % coins_copy.Max();
+                    Console.ReadLine();
+
+                    coins_copy = coins_copy.Where(coin => coin != coins_copy.Max()).ToArray();
+                };
+                if (amountLeft == 0)
+                {
+                    return amountLeft == 0 ? minCoinsRequired : -1;
+                }
+                Console.WriteLine("Trying again but removing the highest valued coin");
+                coins = coins.Where(coin => coin != coins.Max()).ToArray();
+                Console.ReadLine();
+                amountLeft = amount;
+            }
+
+            return amountLeft == 0 ? minCoinsRequired : -1;
+        }
+    }
+}
---
+++ Excercises/LeetCode/StartingApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using LeetCode.Medium;
+
+namespace LeetCode
+{
+    class LeetCode
+    {
+        public static void Main(string[] args)
+        {
+            int choose;
+            Console.WriteLine("Choose Level of diffuculity Level");
+            Console.WriteLine("1 = Easy, 2 = Medium, 3 = Hard");
+            int.TryParse(Console.ReadLine(), out choose);
+            
+
+            switch (choose)
+            {
+                case 1:
+                    Easy();
+                    break;
+                case 2:
+                    Medium();
+                    break;
+                default:
+                    var a  = 0;
+                    break;
+            }
+        }
+
+        public static void Easy() { 
+        }
+
+        public static void Medium() {
+
+            int choose;
+            int[] coins = { 186, 419, 83, 408 };
+            int amount = 6249;
+            Console.WriteLine("Which Problem");
+            choose = int.Parse(Console.ReadLine());
+
+            switch (choose)
+            {
+                case 1:
+                    Console.WriteLine("Answer" + Coin_Change_322.CoinChange( coins, amount).ToString());
+                    Console.ReadLine();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
